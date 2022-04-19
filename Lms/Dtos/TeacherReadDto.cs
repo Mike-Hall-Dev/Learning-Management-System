b23701b8c19abb145c26abd61e576dd6d074dede
Mike Hall <Mike.Hall@vu.com>
2022-04-19 15:18:47 -0500

    Added additional dtos

---
+++ Lms/Dtos/TeacherReadDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lms.Dtos
+{
+    public class TeacherReadDto
+    {
+        [Key]
+        [Required]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        public char MiddleInitial { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+    }
+}
